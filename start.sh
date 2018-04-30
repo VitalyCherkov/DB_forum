d82d36899904c59d996acdb8ea513164520fba45
Vitaly Cherkov <vv-ch@bk.ru>
2018-04-30 22:51:50 +0300

    ready...

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
+sudo apt-get install -y postgresql postgresql-contrib
 
 echo "------------- [OK] Postgres установлен -------------"
 echo "------------- [  ] Java установка      -------------"
